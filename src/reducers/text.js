b8d98b283c9efb28fff9f6dd447f8b7177ca04b8
Jacob Elder <jacob@boundless.com>
2016-08-26 12:50:22 -0400

    Show one random sample per page load

--- src/reducers/text.js
+++ src/reducers/text.js
@@ -1,12 +1,16 @@
+const samples = [
+"202-456-1111",
+"90.9 FM",
+"MAEH08H776",
+]
+
 const default_text = `
 
 Make yourself understood!
 
 Translate any text to the NATO Phonetic Alphabet.
 
-202-456-1111
-
-90.9 FM
+${samples[Math.floor(Math.random()*samples.length)]}
 `
 
 export const text = (state = default_text, action) => {
