0b6b3a5adf1c186a0d9841b4681452c81a595e51
Jacob Elder <jacob@boundless.com>
2016-08-26 12:48:40 -0400

    Default to FAA

--- src/reducers/mode.js
+++ src/reducers/mode.js
@@ -1,4 +1,5 @@
-export const mode = (state = 'codeword', action) => {
+const default_mode = 'faa'
+export const mode = (state = default_mode, action) => {
   switch (action.type) {
     case 'SET_MODE':
       return action.mode
