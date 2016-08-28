3cfa942c6ebafbec29426d0dd88112e8da0d4f1a
Jacob Elder <jacob@boundless.com>
2016-08-27 22:10:05 -0400

    Add localstorage

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { applyMiddleware, createStore } from 'redux';
+import { compose, createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux'
+import persistState from 'redux-localstorage'
 import createLogger from 'redux-logger';
 
 import App from './components/App';
 import './index.css';
 import reducer from './reducers'
 
-const logger = createLogger()
-const store = createStore(reducer, applyMiddleware(logger))
+const enhancer = compose(
+  applyMiddleware(createLogger()),
+  persistState()
+)
+const store = createStore(reducer, enhancer)
 
 ReactDOM.render(
   <Provider store={store}>
