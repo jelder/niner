71e571bf97212e7556c0ccf01efb7508b6cdc43f
Jacob Elder <jacob@boundless.com>
2016-08-31 13:29:01 -0400

    More text changes. CTAs are hard

--- src/reducers/text.js
+++ src/reducers/text.js
@@ -1,13 +1,13 @@
 const default_text = `
 
-Type anything! Convert to phonetics and be understood.
+Type anything in this box. It will be converted to phonetics. Make yourself understood!
 
 +1 (234) 567-8999
 OTM
 ABC
 WTF
 
-It will be saved to your browser's local storage.
+Changes will be saved to your browser's local storage.
 `
 
 export const text = (state = default_text, action) => {
